bfbb930819e9666e1738aed32bc0e1c640a4b258
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2024-02-11 15:41:56 +0530

    install react js and tailwind with creating the page and routing

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+// About.jsx
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      {/* Add more content as needed */}
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+// Home.jsx
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      {/* Add more content as needed */}
+    </div>
+  );
+}
