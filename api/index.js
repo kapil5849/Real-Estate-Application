f701330cc8ce1de3600bbf934fd56216a23f6055
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2024-03-07 04:09:51 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
   console.log('connected to mongoDB');
@@ -9,8 +10,9 @@ mongoose.connect(process.env.MONGO).then(() => {
   console.log(err);
 });
 const app = express();
+app.use(express.json());
 app.listen(3000, () => {
   console.log('Server is running on port 3000!🚀');
 });
-
 app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
