7b6dd01e4ee665c7716c87250e0156d31392f724
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2024-04-03 01:59:51 +0530

    createsign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
+
 export const signup = async (req, res, next) => { // add next as a parameter
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -12,3 +14,18 @@ export const signup = async (req, res, next) => { // add next as a parameter
         next(error); // pass the error to the error handler
     }
 };
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return errorHandler(404, 'User not found! ');
+        const validPassword = await bcryptjs.compare(password, validUser.password);
+        if (!validPassword) return errorHandler(401, 'Wrong credentials! ');
+        const token = jwt.sign({id: validUser._id},process.env.JWT_SECRET);
+        const {password: pass, ...rest} = validUser._doc;
+        res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
+    }
+    catch (error) {
+        next(error);
+    }
+}
