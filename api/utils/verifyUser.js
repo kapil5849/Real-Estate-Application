fcf920189bc8d3637e6aad903d2250a12e9d8529
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2024-04-16 00:55:07 +0530

    complete create listing page ui

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -3,8 +3,6 @@ import { errorHandler } from './error.js';
 
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.access_token;
-
-    // console.log("Token:", token);
     
     if (!token) return next(errorHandler(401, 'Unauthorized'));
 
