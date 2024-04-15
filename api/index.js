86ae06438efdc2241f73bea5088dc49b1fe308e0
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2024-04-15 16:00:05 +0530

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -3,8 +3,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-dotenv.config();
+import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+dotenv.config();
+
 mongoose.connect(process.env.MONGO).then(() => {
   console.log('connected to mongoDB');
 }).catch((err) => {
@@ -16,8 +18,11 @@ app.use(cookieParser());
 app.listen(3000, () => {
   console.log('Server is running on port 3000!🚀');
 });
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use('/api/listing', listingRouter);
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,6 @@
-export const errorHandler = (statusCode,message)=>{ // create a function that takes in a status code and a message
-    const error = new Error(); // create a new error object
-    error.statusCode = statusCode; // set the status code of the error object
-    error.message = message; // set the message of the error object
-    return error; // return the error object
-}
+export const errorHandler = (statusCode, message) => {
+    const error = new Error();
+    error.statusCode = statusCode;
+    error.message = message;
+    return error;
+};
