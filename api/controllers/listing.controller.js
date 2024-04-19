695773e0c38464e87f7af45d5e37fa7870f4138d
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2024-04-20 00:28:25 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -26,3 +26,48 @@ export const deleteListing = async (req, res, next) => {
     }
 };
 
+// export const updateListing = async (req, res, next) => {
+//     const listing = await Listing.findById(req.params.id);
+//     if (!listing) {
+//         return next(errorHandler(404, 'Listing not found!'));
+//     }
+//     if (req.user.id !== listing.userRef) {
+//         return next(errorHandler(401, 'You can only update your own listings!'));
+//     }
+//     try {
+//         const updatedListing = await Listing.findByIdAndUpdate(
+//             req.params.id,
+//             req.body,
+//             { new: true }
+//         );
+//         res.status(200).json(updatedListing);
+//     } catch (error) {
+//         next(error);
+//     }
+// };
+
+
+export const updateListing = async (req, res, next) => {
+    let listing;
+    try {
+        listing = await Listing.findById(req.params.id);
+    } catch (error) {
+        return next(errorHandler(500, 'Error finding listing'));
+    }
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not found!'));
+    }
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'You can only update your own listings!'));
+    }
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
+};
