0fc1192faebc38f2befbbfc565e16423d621ff61
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2024-04-21 01:54:45 +0530

    add contact landlord functionality to the listing page

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -39,12 +39,12 @@ export default function Contact({ listing }) {
             className='w-full border p-3 rounded-lg'
           ></textarea>
 
-          <a
-          href={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+          <Link
+          to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
           className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
           >
             Send Message          
-          </a>
+          </Link>
         </div>
       )}
     </>
