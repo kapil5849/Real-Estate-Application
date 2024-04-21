cc9cfcbe31da39155ac437cf262f793d61e601b6
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2024-04-21 23:53:06 +0530

    create the listing item component and show listings

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ListingItem from '../components/ListingItem';
+
 export default function Search() {
     const navigate = useNavigate();
     const [sidebardata, setSidebardata] = useState({
@@ -137,7 +139,7 @@ export default function Search() {
                             <span>Sale</span>
                         </div>
                         <div className='flex gap-2'>
-                            <input type='checkbox' id='offer' className='w-5' onChange={handleChange} checked={sidebardata.offer}/>
+                            <input type='checkbox' id='offer' className='w-5' onChange={handleChange} checked={sidebardata.offer} />
                             <span>Offer</span>
                         </div>
                     </div>
@@ -154,7 +156,7 @@ export default function Search() {
                     </div>
                     <div className='flex items-center gap-2'>
                         <label className='font-semibold'>Sort:</label>
-                        <select id='sort_order' className='border rounded-lg p-3' onChange={handleChange}  defaultValue={'created_at_desc'} >
+                        <select id='sort_order' className='border rounded-lg p-3' onChange={handleChange} defaultValue={'created_at_desc'} >
                             <option>Price high to low</option>
                             <option>Price low to hight</option>
                             <option>Latest</option>
@@ -166,8 +168,21 @@ export default function Search() {
                     </button>
                 </form>
             </div>
-            <div className=''>
+            <div className='flex-1'>
                 <h1 className='text-3xl font-semibold border-b p-3 text-slate-700 mt-5'>Listing results:</h1>
+                <div className="p-7 flex flex-wrap gap-4">
+                    {!loading && listings.length === 0 && (
+                        <p className="text-xl text-slate-700">No listing found!</p>
+                    )}
+                    {loading && (
+                        <p className="text-xl text-slate-700 text-center w-full">Loading...</p>
+                    )}
+                    {!loading &&
+                        listings &&
+                        listings.map((listing) => (
+                            <ListingItem key={listing._id} listing={listing} />
+                        ))}
+                </div>
             </div>
         </div>
     );
